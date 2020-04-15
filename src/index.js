c079deb7bec6470f7d7e63efc0a18e7bc7b25854
Adler Coelho Santos <adler.santos@ftd.com.br>
2020-04-14 22:47:38 -0300

    Criando formulário de login.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import './styles.scss';
 import 'bootstrap';
 
-import styles from './index.module.css';
+// import styles from './index.module.css';
 
-const h1 = document.querySelector('h1');
-h1.classList.add(styles.h1);
+// const h1 = document.querySelector('h1');
+// h1.classList.add(styles.h1);
 
-async function print() {
-  const { greet } = await import('./greet');
-  const response = await greet('John Doe');
-  console.log(response);
-}
+// async function print() {
+//   const { greet } = await import('./greet');
+//   const response = await greet('John Doe');
+//   console.log(response);
+// }
 
-print();
+// print();
